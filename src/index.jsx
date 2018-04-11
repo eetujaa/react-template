f99b199dfbf599a9bf97b01497e8a17a2ed2d9c3
Eetu Jääsola <eetujaasola@Eetu-MacBook-Air.local>
2018-04-11 15:46:25 +0300

    video

--- src/index.jsx
+++ src/index.jsx
@@ -9,12 +9,19 @@ import './assets/stylesheets/style.css'
 
 const Hello = () => (
   <div>
+  
     <h1>BIM BOM! Banaani make hehehehehehe!!!</h1>
+  
     <h2>Virallinen meemisivu lul</h2>
-    <div><img src="https://ih1.redbubble.net/image.409410989.4197/flat,750x1000,075,t.u1.jpg" width="320" height="420"></img>
+  
+    <div>
+      <img src="https://ih1.redbubble.net/image.409410989.4197/flat,750x1000,075,t.u1.jpg" width="320" height="420"></img>
   </div>
-  <div><video src="https://youtu.be/_0C2rQQiSss" width="200" height="200" controls>Ei toimi</video>
+  
+  <div>
+  <iframe width="454" height="280" src="https://www.youtube.com/embed/_0C2rQQiSss" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
   </div>
+  
   </div>
 );
 
