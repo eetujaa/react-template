97267e72047e8aeda480297f2850ddd20f1ef837
Eetu Jääsola <eetujaasola@Eetu-MacBook-Air.local>
2018-04-11 13:46:29 +0300

    kuva

--- src/index.jsx
+++ src/index.jsx
@@ -8,7 +8,14 @@ import './assets/stylesheets/style.css'
 /****** ADD YOUR CODE AFTER THIS LINE ******/
 
 const Hello = () => (
-  <div>BIM BOM! Banaani make hehehehehehe!!!</div>
+  <div>
+    <h1>BIM BOM! Banaani make hehehehehehe!!!</h1>
+    <h2>Virallinen meemisivu lul</h2>
+    <div><img src="https://ih1.redbubble.net/image.409410989.4197/flat,750x1000,075,t.u1.jpg" width="320" height="420"></img>
+  </div>
+  <div><video src="https://youtu.be/_0C2rQQiSss" width="200" height="200" controls>Ei toimi</video>
+  </div>
+  </div>
 );
 
 class App extends Component {
