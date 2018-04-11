cd65184b63f1fd33d116ede1d3ca2f5b02f8d1ab
Eetu Jääsola <eetujaasola@Eetu-MacBook-Air.local>
2018-04-11 15:49:31 +0300

    hah

--- src/index.jsx
+++ src/index.jsx
@@ -12,7 +12,7 @@ const Hello = () => (
   
     <h1>BIM BOM! Banaani make hehehehehehe!!!</h1>
   
-    <h2>Virallinen meemisivu lul</h2>
+    <h2>Extra spicy</h2>
   
     <div>
       <img src="https://ih1.redbubble.net/image.409410989.4197/flat,750x1000,075,t.u1.jpg" width="320" height="420"></img>
