e18ea20dbfc9977d0f4e4469b539f92ee0c7fcf2
Eetu Jääsola <eetujaasola@Eetu-MacBook-Air.local>
2018-04-11 10:38:57 +0300

    makaroonit on hyviä

--- src/index.jsx
+++ src/index.jsx
@@ -8,7 +8,7 @@ import './assets/stylesheets/style.css'
 /****** ADD YOUR CODE AFTER THIS LINE ******/
 
 const Hello = () => (
-  <div>BIM BOM!</div>
+  <div>BIM BOM! Banaani make hehehehehehe!!!</div>
 );
 
 class App extends Component {
